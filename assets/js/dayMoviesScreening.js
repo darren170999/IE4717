8aafaec3055e52ab2d7ab60f8da02cb3b2a6ebca
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-10-06 16:18:32 +0800

    CSS Updates

--- assets/js/dayMoviesScreening.js
+++ assets/js/dayMoviesScreening.js
@@ -1,21 +1,3 @@
-// // script.js
-// const tabButtons = document.querySelectorAll('.tab-button');
-// const dayMovies = document.querySelectorAll('.day-movies');
-
-// tabButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         // Hide all movie listings
-//         dayMovies.forEach(day => {
-//             day.style.display = 'none';
-//         });
-
-//         // Show the selected day's movies
-//         const selectedDay = button.getAttribute('data-day');
-//         const selectedDayMovies = document.getElementById(selectedDay + '-movies');
-//         selectedDayMovies.style.display = 'block';
-//     });
-// });
-// dayMoviesScreening.js
 document.addEventListener('DOMContentLoaded', function() {
     const tabButtons = document.querySelectorAll('.tab-button');
     const dayMovies = document.querySelectorAll('.day-movies');
