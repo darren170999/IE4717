4f7deb1ea044d52ee38bac306664766007f9548f
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-10-28 11:26:52 +0800

    Nicer UI for admin.php

--- admin.php
+++ admin.php
@@ -10,51 +10,53 @@ if (isset($_SESSION['valid_user'])) {
 <html lang="en">
 <head>
     <title>Pure Frames</title>
-	<meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/adminStyle.css">
 </head>
 <body>
-    <h1>Upload Advertisements</h1>
-    <form action="createAds.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-    <h1>Upload Upcoming Movie Posters</h1>
-    <form action="createMovies.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submitPoster">
-    </form>
-    <h1>Upload Long Posters</h1>
-    <form action="createLongPosters.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submitPoster">
-    </form>
-    <h1>Upload Current Movies</h1>
-    <form action="createCurrentMovies.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload"><br>
-        Movie title
-        <input type="text" name="movie_title" id="movie_title"><br>
-        sypnopsis
-        <input type="text" name="sypnopsis" id="sypnopsis"><br>
-        casts
-        <input type="text" name="casts" id="casts"><br>
-        date
-        <input type="date" name="screening_date" id="screening_date"><br>
-        <!-- timings
-        <input type="time" name="screening_time" id="screening_time"><br> -->
-        price
-        <input type="number" name="price" id="price"><br>
-        ratings
-        <input type="number" name="ratings" id="ratings"><br>
-        hall_id
-        <input type="number" name="hall_id" id="hall_id"><br>
-        
-        <input type="submit" value="Upload Image" name="submitMovie">
-    </form>
+    <div class="form-container">
+        <h1>Upload Advertisements</h1>
+        <form action="createAds.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+        <h1>Upload Upcoming Movie Posters</h1>
+        <form action="createMovies.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submitPoster">
+        </form>
+        <h1>Upload Long Posters</h1>
+        <form action="createLongPosters.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submitPoster">
+        </form>
+        <h1>Upload Current Movies</h1>
+        <form action="createCurrentMovies.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload"><br>
+            Movie title
+            <input type="text" name="movie_title" id="movie_title"><br>
+            Synopsis
+            <input type="text" name="sypnopsis" id="sypnopsis"><br>
+            Casts
+            <input type="text" name "casts" id="casts"><br>
+            Date
+            <input type="date" name="screening_date" id="screening_date"><br>
+            <!-- Timings
+            <input type="time" name="screening_time" id="screening_time"><br> -->
+            Price
+            <input type="number" name="price" id="price"><br>
+            Ratings
+            <input type="number" name="ratings" id="ratings"><br>
+            Hall ID
+            <input type="number" name="hall_id" id="hall_id"><br>
+
+            <input type="submit" value="Upload Image" name="submitMovie">
+        </form>
+    </div>
 </body>
 </html>
 <?php
